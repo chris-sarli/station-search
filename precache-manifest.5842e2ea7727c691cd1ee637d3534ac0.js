ad875707aa928189af067ca0eba0d5040f60a224
chris-sarli <chris-sarli@users.noreply.github.com>
2021-12-24 20:40:00 +0000

    deploy: ad196e96c79dc0f7e1b46a9da418559d74e1e6d5

---
+++ precache-manifest.5842e2ea7727c691cd1ee637d3534ac0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e137be19e843ceac4e558ea6fdbba6b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4380786b34a187f1a9d",
+    "url": "/static/css/main.1211e0fb.chunk.css"
+  },
+  {
+    "revision": "967db7ac9f49bfdbb834",
+    "url": "/static/js/2.05786c63.chunk.js"
+  },
+  {
+    "revision": "a4380786b34a187f1a9d",
+    "url": "/static/js/main.acc9ec9e.chunk.js"
+  },
+  {
+    "revision": "8722febb800454f52f1c",
+    "url": "/static/js/runtime-main.e191ea03.js"
+  }
+]);
